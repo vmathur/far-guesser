790b05e8012f7f6dc8f8d09f0e02fa6d62b7fe9d
vmathur <vmathur@users.noreply.github.com>
2025-03-06 22:42:00 -0800

    prod manifest

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -4,8 +4,10 @@ export async function GET() {
   const config = {
     accountAssociation: {
       header: "eyJmaWQiOjE3NTkyLCJ0eXBlIjoiY3VzdG9keSIsImtleSI6IjB4YUQ3YjNlMTBhZDc3NEMxOTM4ZGFDQzEyNGYxMTk4M2YwRTRkQTM3MSJ9",
-      payload: "eyJkb21haW4iOiI1Njg5MGU0NTg3MjQubmdyb2suYXBwIn0",
-      signature: "MHgzNzIyMjRhMWQyYTk4OGVkMWNjMWRjMTA1NzgwOWRiZTI4NDBkYzIwOTU3Yzc4ZWE0NjIwZjYwZmM2NzI0OTUxMGI5YTE2NGNkOWQ0MGZmM2U0ZmUzNDI2M2QzNzdmZTU1Mzg1NjBlNGZlODc2MTljY2QwM2RmY2U3NDcwMDc1YTFi"
+      payload: "eyJkb21haW4iOiJmYXItZ3Vlc3Nlci52ZXJjZWwuYXBwIn0",
+      // payload: "eyJkb21haW4iOiI1Njg5MGU0NTg3MjQubmdyb2suYXBwIn0",
+      signature: "MHgxNmM4NjFhMzY3MGZkYWViZjM0MGMzZWRmNjg3OGFjMGY4NjMzYzA5MmFlYzFiM2QwNDYyNWZhZmM2NDViNjU3NWIxOTExN2MxMmFlMWYyZGUwMzc3OGE2ODBkOTZhNzIwYzdjZGFhYzg4NTk5ZWEzMjJmYzgxNDlhMmZjNjgwZDFj"
+      // signature: "MHgzNzIyMjRhMWQyYTk4OGVkMWNjMWRjMTA1NzgwOWRiZTI4NDBkYzIwOTU3Yzc4ZWE0NjIwZjYwZmM2NzI0OTUxMGI5YTE2NGNkOWQ0MGZmM2U0ZmUzNDI2M2QzNzdmZTU1Mzg1NjBlNGZlODc2MTljY2QwM2RmY2U3NDcwMDc1YTFi"
     },
     frame: {
       version: "1",
